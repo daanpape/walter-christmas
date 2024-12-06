403b52c1bb5f47e6d0b959c7c71b05e58f938110
Daan Pape <daan@dptechnics.com>
2024-12-06 11:22:06 +0100

    Programmed a running effect with changing colors

--- walter-christmas.ino
+++ walter-christmas.ino
@@ -3,7 +3,7 @@
 /**
  * @brief The number of connected LEDs.
  */
-#define NUM_LEDS 100
+#define NUM_LEDS 200
 
 /**
  * @brief The data pin on which the LEDs are connected.
@@ -11,11 +11,39 @@
 #define DATA_PIN 10
 
 /**
+ * @brief The number of lights in a single section that lights up.
+ */
+#define SECTION_SIZE 10
+
+/**
  * @brief The array of WS2812B LEDs.
  */
 CRGB leds[NUM_LEDS];
 
-int cursor = 0;
+/**
+ * @brief The cursor of where we are lighting up the section.
+ */
+int cursor = SECTION_SIZE;
+
+/**
+ * @brief The direction in which the light runs.
+ */
+bool direction = true;
+
+/**
+ * @brief The current RED channel color.
+ */
+uint8_t red = 0;
+
+/**
+ * @brief The current GREEN channel color.
+ */
+uint8_t green = 0;
+
+/**
+ * @brief The current BLUE channel color.
+ */
+uint8_t blue = 255;
 
 void setup() {
   Serial.begin(115200);
@@ -30,12 +58,34 @@ void loop() {
     leds[i] = CRGB(0,0,0);
   }
 
-  leds[cursor] = CRGB(255,255,255);
-  leds[cursor + 1] = CRGB(255,0,0);
-  leds[cursor + 2] = CRGB(0,255,0);
-  leds[cursor + 3] = CRGB(0,0,255);
+  for(int i = 0; i < SECTION_SIZE; ++i) {
+    leds[cursor - i] = CRGB(red, green, blue);
+  }
+  
   FastLED.show();
 
-  cursor += 1;
-  delay(100);
+  if(direction) {
+    cursor += 1;
+    if(cursor == NUM_LEDS) {
+      direction = false;
+      cursor = NUM_LEDS - 1;
+      red = random(0, 256);
+      green = random(0, 256);
+      blue = random(0, 256);
+      Serial.printf("New color: (%d, %d, %d)\n", (int) red, (int) green, (int) blue);
+    }
+  } else {
+    cursor -= 1;
+
+    if(cursor == SECTION_SIZE - 1) {
+      direction = true;
+      cursor = SECTION_SIZE;
+      red = random(0, 256);
+      green = random(0, 256);
+      blue = random(0, 256);
+      Serial.printf("New color: (%d, %d, %d)\n", (int) red, (int) green, (int) blue);
+    }
+  }
+  
+  delay(20);
 }
